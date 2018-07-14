9b3f6a4da1cffc18b80fac417512e7ce307c4e35
Pack Fan 1031 <ocskier@gmail.com>
2018-07-14 10:08:39 -0400

    almost final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,9 +5,17 @@ var topics = ["Basketball","Baseball","Football","Hockey","Golf","Tennis","Swimm
 
 
 var newBtnGrp = $('<div class="btn-group btn-group-sm mybtngrp" role="group" aria-label="Basic example">');
-for (i=0; i <topics.length;i++) {
+var addNewBtnGrp = $('<div class="btn-group btn-group-sm myaddbtngrp" role="group" aria-label="Basic example">');
+
+var middle = Math.floor(topics.length/2);
+for (i=0; i <middle;i++) {
     newBtnGrp.append('<button type="button" class="btn btn-info topic-btn" style="font-size: 14px; margin: 2px 3px;" data-topic="'+topics[i]+'">'+topics[i]+'</button>');
 }
+newBtnGrp.append('<br>');
+for (i=middle; i <topics.length;i++) {
+    newBtnGrp.append('<button type="button" class="btn btn-info topic-btn" style="font-size: 14px; margin: 2px 3px;" data-topic="'+topics[i]+'">'+topics[i]+'</button>');
+}
+
 var lengthGifList;
 
 function queryGiphy (cat) {
@@ -32,6 +40,7 @@ function queryGiphy (cat) {
 }
 
 $('#btn-container').append(newBtnGrp);
+$('#addbtn-container').append(addNewBtnGrp);
 
 $(".mybtngrp").on("click",".topic-btn",function() {
     $("#gif-container").empty();
@@ -39,13 +48,20 @@ $(".mybtngrp").on("click",".topic-btn",function() {
     queryGiphy(chosenTopic);
 });
 
+$(".myaddbtngrp").on("click",".topic-btn",function() {
+    $("#gif-container").empty();
+    var chosenTopic = $(this).attr("data-topic");
+    queryGiphy(chosenTopic);
+});
+
 $("#searchBtn").on("click", function () {
     var newTopic = $('input').val(); 
-    queryGiphy(newTopic);
     setTimeout(function() {    
         if (!(newTopic == "")&&!(lengthGifList==0)) {
+            $("#gif-container").empty();
+            queryGiphy(newTopic);
             var newBtn = $('<button type="button" class="btn btn-info topic-btn" style="font-size: 14px; margin: 2px 3px;" data-topic="'+newTopic+'">'+newTopic+'</button>');
-            $(".mybtngrp").append(newBtn);
+            $(".myaddbtngrp").append(newBtn);
         }
         },1200);
     $('input').val("");
@@ -53,4 +69,19 @@ $("#searchBtn").on("click", function () {
 
 $("#clearBtn").on("click", function () {
     $("#gif-container").empty();
+});
+
+$("iframe").on("click", function() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
 });
