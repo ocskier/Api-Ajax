ba91c50d976c14b6729ae2dc8868048abae1051e
Pack Fan 1031 <ocskier@gmail.com>
2018-07-15 19:06:53 -0400

    almost final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,25 @@ for (i=middle; i <topics.length;i++) {
     newBtnGrp.append('<button type="button" class="btn btn-info topic-btn" style="font-size: 14px; margin: 2px 3px;" data-topic="'+topics[i]+'">'+topics[i]+'</button>');
 }
 
-var lengthGifList;
+var lengthGifSearch;
+var lengthGifList=0;
+
+function makeBtn () {
+    var btn = $("<button></button>");
+    btn.attr("id","index-"+lengthGifList+'"');
+    btn.attr("data", lengthGifList);
+    btn.attr("class","checkbox");
+    btn.text("X");
+    return btn;
+}
+function makeFav () {
+    var btn = $("<button></button>");
+    btn.attr("id","index-"+lengthGifList+'"');
+    btn.attr("data", lengthGifList);
+    btn.attr("class","favorite");
+    btn.append('<img src="assets/images/favsymbol.jpg">');
+    return btn;
+}
 
 function queryGiphy (cat) {
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -27,13 +45,15 @@ function queryGiphy (cat) {
         method: "GET"
         }).then(function(response) {
         console.log(response);
-        console.log(response.data.length);
-        lengthGifList=response.data.length;
-        console.log(lengthGifList);
+        lengthGifSearch=response.data.length;
         for (i=0;i<response.data.length;i++){
-            var newGifDiv = $('<div class="gif-card">');
+            lengthGifList++;
+            var newGifDiv = $('<div class="gif-card" id="index-'+lengthGifList+'">');
+            newGifDiv.append(makeFav());
+            newGifDiv.append(makeBtn());
+            newGifDiv.append('<img src="'+response.data[i].images.original.url+'" width="270" height="200" frameBorder="0" class = "my-img" data-animate="'+response.data[i].images.original.url+'" data-still="'+response.data[i].images.original_still.url+'" data-state="animate" allowFullScreen></iframe>');
+            newGifDiv.append('<p>'+response.data[i].title+'</p>');
             newGifDiv.append('<p>Rating: '+response.data[i].rating+'</p>');
-            newGifDiv.append('<iframe src="'+response.data[i].embed_url+'" width="260" height="180" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
             $("#gif-container").append(newGifDiv);
         }
     });            
@@ -43,7 +63,7 @@ $('#btn-container').append(newBtnGrp);
 $('#addbtn-container').append(addNewBtnGrp);
 
 $(".mybtngrp").on("click",".topic-btn",function() {
-    $("#gif-container").empty();
+    
     var chosenTopic = $(this).attr("data-topic");
     queryGiphy(chosenTopic);
 });
@@ -56,22 +76,42 @@ $(".myaddbtngrp").on("click",".topic-btn",function() {
 
 $("#searchBtn").on("click", function () {
     var newTopic = $('input').val(); 
-    setTimeout(function() {    
-        if (!(newTopic == "")&&!(lengthGifList==0)) {
-            $("#gif-container").empty();
-            queryGiphy(newTopic);
-            var newBtn = $('<button type="button" class="btn btn-info topic-btn" style="font-size: 14px; margin: 2px 3px;" data-topic="'+newTopic+'">'+newTopic+'</button>');
-            $(".myaddbtngrp").append(newBtn);
-        }
-        },1200);
+    if (!(newTopic == "")) {
+        queryGiphy(newTopic);
+        setTimeout(function() { 
+            if (!(lengthGifSearch===0)) {
+                var newBtn = $('<button type="button" class="btn btn-info topic-btn" style="font-size: 14px; margin: 2px 3px;" data-topic="'+newTopic+'">'+newTopic+'</button>');
+                $(".myaddbtngrp").append(newBtn);
+            }
+        },2500);
+    }
     $('input').val("");
 }); 
+
+$(".gif-class").on("click", ".checkbox", function () {
+    var indexClicked = $(this).attr("data");
+    $("#index-"+indexClicked).remove();
+});
+
+$(".gif-class").on("click", ".favorite", function () {
+    var isFavsActivated = $("#fav-container").attr("favs-activated"); 
+    if (isFavsActivated == "false") {
+        $("#fav-container").append('<h2 style="color: black;">Favorites</h2>');
+        $("#fav-container").attr("favs-activated","true");
+    }
+    var indexClicked = $(this).attr("data");
+    this.remove();
+    var copyCard = $("#index-"+indexClicked);
+    $("#index-"+indexClicked).remove();
+    $("#fav-container").append(copyCard);
+});
 
 $("#clearBtn").on("click", function () {
     $("#gif-container").empty();
 });
 
-$("iframe").on("click", function() {
+$(".gif-class").on("click",".my-img",function() {
+
     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
     var state = $(this).attr("data-state");
     // If the clicked image's state is still, update its src attribute to what its data-animate value is.
